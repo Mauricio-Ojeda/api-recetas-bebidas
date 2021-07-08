ac106fe61eca895bbc7578cbcb2be295eaaff94c
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-07-08 08:55:03 -0300

    validacion al form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,11 +7,15 @@ const Form = () => {
 	// use context
 	const {categorys} = useContext(CategoriaContext);
 	const {setSearch, setConsultar} = useContext(RecetasContext);
+	const [error, setError] = useState(false)
 
 	const [dataForm, setdataForm] = useState({
 		name: '',
 		category: ''
 	})
+
+	// destructuring dataForm
+	const { name, category } = dataForm;
 
 	// handle changes form
 
@@ -28,6 +32,17 @@ const Form = () => {
 
 		e.preventDefault();
 
+		// Validation form
+
+		if ( name.trim() === '' && category.trim() === '') {
+			
+			setError(true);
+			return;			
+		}
+
+		// upDate Error state
+		setError(false);
+
 		setSearch(dataForm);
 		setConsultar(true);
 	}
@@ -40,27 +55,48 @@ const Form = () => {
 			>
 				<fieldset className="text-center">
 					<legend className="font-weight-bold">Busca Bebidas por Categorias o Ingredientes</legend>
+					{ ( error ) ? <p className="error"> Debes Completar Alguno de los Campos</p> : null }
 				</fieldset>
 
 				<div className="row mt-5">
 					<div className="col-md-4 mt-3">
-						<input 
-							name="name"
-							className="form-control"
-							type="text"
-							placeholder="Buscar por Ingredientes"
-							onChange={handleChange}
-						/>
+						{ ( error ) ? 
+							<input 
+								name="name"
+								className="form-control errorInput"
+								type="text"
+								placeholder="Buscar por Ingredientes"
+								onChange={handleChange}
+							/> :
+								<input 
+									name="name"
+									className="form-control"
+									type="text"
+									placeholder="Buscar por Ingredientes"
+									onChange={handleChange}
+								/>
+						}
 					</div>
 					<div className="col-md-4 mt-3">
-						<select
-							className="form-control"
-							name="category"
-							onChange={handleChange}
-						>
-							<option value=""> Selecciona una Categoria </option>
-							{categorys.map((category, index) =>( <option key={index} value={category.strCategory}>{category.strCategory}</option>))}
-						</select>
+						{ ( error ) ? 
+							<select
+								className="form-control errorInput"
+								name="category"
+								onChange={handleChange}
+							>
+								<option value=""> Selecciona una Categoria </option>
+								{categorys.map((category, index) =>( <option key={index} value={category.strCategory}>{category.strCategory}</option>))}
+							</select> :
+							
+							<select
+								className="form-control"
+								name="category"
+								onChange={handleChange}
+							>
+								<option value=""> Selecciona una Categoria </option>
+								{categorys.map((category, index) =>( <option key={index} value={category.strCategory}>{category.strCategory}</option>))}
+							</select>
+						}
 					</div>
 
 					<div className="col-md-4 mt-3">
